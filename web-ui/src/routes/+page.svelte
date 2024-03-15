b1480805cf6f53a94820cbde613306dd0502fd45
Zach Champion <zachchampion79@gmail.com>
2024-03-15 04:32:01 -0400

    Use md3 components

--- web-ui/src/routes/+page.svelte
+++ web-ui/src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button, TextFieldOutlined } from 'm3-svelte';
+
+	function connect() {
+		alert('test');
+	}
+
+	let url = 'ws://balancebot:80';
+</script>
+
+<div id="container">
+	<h1>BalanceBot Web UI</h1>
+	<p>Welcome to the BalanceBot Web UI. Connect to your bot's WebSocket server below.</p>
+	<div style="height: 48px;"></div>
+	<form on:submit|preventDefault={connect}>
+		<div id="form-container">
+			<TextFieldOutlined bind:value={url} name="WebSocket URL" />
+			<Button type="filled">Connect</Button>
+		</div>
+	</form>
+</div>
+
+<style>
+	#container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		width: 100vw;
+	}
+
+	#form-container {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+</style>
